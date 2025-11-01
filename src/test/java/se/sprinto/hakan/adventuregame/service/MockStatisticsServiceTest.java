d0553ab2ecbdf8a0d61676f299614ca6be12f15f
katttiger <katttiger@gmail.com>
2025-11-01 13:18:45 +0100

    Tested loadStatistics with Mockito.

--- src/test/java/se/sprinto/hakan/adventuregame/service/MockStatisticsServiceTest.java
+++ src/test/java/se/sprinto/hakan/adventuregame/service/MockStatisticsServiceTest.java
@@ -1,11 +1,13 @@
 package se.sprinto.hakan.adventuregame.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import se.sprinto.hakan.adventuregame.dao.FileStatisticsDao;
 import se.sprinto.hakan.adventuregame.model.Statistics;
@@ -26,21 +28,36 @@ class MockStatisticsServiceTest {
 
     @BeforeEach
     void setUp() {
-        testStatistics.clear();
         testStatistics = new ArrayList<>();
+        //Add the below to list testStatistics
         Statistics fakeStat0 = new Statistics("Player11", 4);
         Statistics fakeStat1 = new Statistics("Player22", 10);
         Statistics fakestat2 = new Statistics("Player66", 2);
+
+        testStatistics.add(fakeStat0);
+        testStatistics.add(fakeStat1);
+        testStatistics.add(fakestat2);
     }
 
     @Test
     @DisplayName("loadAll-method returns unsorted list")
     void loadStatistics() {
+
         //Returnera en hårdkodad lista med statistics-object (osorterad)
+        Mockito.when(fileStatisticsDao.loadAll()).thenReturn(testStatistics);
         List<Statistics> scoreTestList = fileStatisticsDao.loadAll();
+        if (scoreTestList.isEmpty()) {
+            Assertions.fail();
+        }
+        for (int i = 0; i < scoreTestList.size(); i++) {
+            assert scoreTestList.get(i).getScore() == testStatistics.get(i).getScore() && scoreTestList.get(i).getPlayerName() == testStatistics.get(i).getPlayerName();
+        }
+
+
     }
 
     @Test
+    @DisplayName("Method getSortedStatistics returns sorted list.")
     void getSortedStatistics() {
 
 
